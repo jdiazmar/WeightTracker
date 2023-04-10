93c689aa9cf2374aa86ea95eea3c2914cb402b5c
Josmar Diaz <jdm.begreat28@gmail.com>
2023-04-10 19:39:25 -0400

    Added Paths to Pages on Button Feature on NavBar

--- weight-tracker/src/Components/NavBar/NavBar.jsx
+++ weight-tracker/src/Components/NavBar/NavBar.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import './NavBar.scss';
 
 const NavBar = () => {
     return ( 
         <div className='navBar' >
-            <ul>
-                <li>
-                    <button>HOME</button>
-                </li>
-                <li>
-                    <button>TRACKER</button>
-                </li>
-                <li>
-                    <button>ABOUT</button>
-                </li>
-            </ul>
+            <nav>
+                <NavLink exact='true' activeclassname='active' to='/'  >
+                    Home
+                </NavLink>
+            </nav>
+            <nav>
+                <NavLink exact='true' activeclassname='active' to='/tracker' >
+                    Tracker
+                </NavLink>
+            </nav>
+            <nav>
+                <NavLink exact='true' activeclassname='active' to='/about' >
+                    About
+                </NavLink>
+            </nav>
         </div>
      );
 }
